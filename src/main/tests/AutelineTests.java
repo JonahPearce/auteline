5aa54d644a407df408fb0b3502c046aa1507a4cb
Torin Sparks <tsparks9038@conestogac.on.ca>
2022-10-21 16:05:50 -0400

    added pin verification tests.

--- src/main/tests/AutelineTests.java
+++ src/main/tests/AutelineTests.java
@@ -4,11 +4,40 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import main.java.ATM;
+import main.java.BankDatabase;
+
 class AutelineTests {
 
 	@Test
 	void test() {
 		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testValidAcct() {
+		int id = 11111;
+		int pin = 11111;
+		boolean val;
+		
+		BankDatabase bankDatabase = null;
+		
+		val = bankDatabase.authenticateUser(id, pin);
+		
+		assertTrue(val);
+	}
+	
+	@Test
+	void testInvalidAcct() {
+		int id = 22222;
+		int pin = 11111;
+		boolean val;
+		
+		BankDatabase bankDatabase = null;
+		
+		val = bankDatabase.authenticateUser(id, pin);
+		
+		assertFalse(val);
 	}
 
 }
