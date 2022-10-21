aee186feefe7474f6a2e42744113e9e55bb57bf2
Jonah Pearce <Jpearce1558@conestogac.on.ca>
2022-10-21 15:34:53 -0400

    JUnit Test Case file and libraries included.

---
+++ src/main/tests/AutelineTests.java
@@ -0,0 +1,14 @@
+package main.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AutelineTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
--- src/module-info.java
+++ src/module-info.java
@@ -8,4 +8,5 @@
 module auteline {
 	requires java.sql;
 	requires java.desktop;
+	requires org.junit.jupiter.api;
 }
